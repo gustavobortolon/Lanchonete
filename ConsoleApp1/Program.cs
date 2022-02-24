af85b18288aca410df66068852a31f2b3d786f21
Gustavo <gustavo_bortolon@hotmail.com>
2022-02-24 11:53:10 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 - cachorro quente = 4,00 " +
+                "2 - X-Salada = 4,50 " +
+                "3 - X-Bacon = 5,00 " +
+                "4 - Torrada Simples = 2,00 " +
+                "5 - Refrigerante = 1,50 ");
+            string[] valores = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(valores[0]);
+            int quantidade = int.Parse(valores[1]);
+
+            double total;
+            if (codigo == 1)
+            {
+                total = quantidade * 4.0;
+            }
+            else if (codigo == 2)
+            {
+                total = quantidade * 4.5;
+            }
+            else if (codigo == 3)
+            {
+                total = quantidade * 5.0;
+            }
+            else if (codigo == 4)
+            {
+                total = quantidade * 2.0;
+            }
+            else
+            {
+                total = quantidade * 1.5;
+            }
+
+            Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
